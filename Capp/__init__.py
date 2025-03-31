5af8b158941db1cf0bf05ed227fc4513b1eed510
Erik-g-f <erikgfines@gmail.com>
2025-03-31 14:57:44 +0200

    Stor oppdatering

--- Capp/__init__.py
+++ Capp/__init__.py
@@ -14,3 +14,25 @@ application.register_blueprint(Carbon_app)
 application.register_blueprint(register)
 application.register_blueprint(About_us)
 
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+import os
+
+application = Flask(__name__)
+
+# application.config['SECRET_KEY'] = os.environ['SECRET_KEY']  
+application.config['SECRET_KEY'] = '3oueqkfdfas8ruewqndr8ewrewrouewrere44554'
+
+application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
+application.config['SQLALCHEMY_BINDS'] ={'transport': 'sqlite:///transport.db'}
+
+
+db = SQLAlchemy(application)
+bcrypt = Bcrypt(application)
+login_manager= LoginManager(application)
+login_manager.login_view = 'users.login'
+login_manager.login_message_category = 'info'
+
+
