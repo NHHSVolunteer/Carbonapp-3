295893730cf208ae9466033a2afcec1415485ac2
Erik-g-f <erikgfines@gmail.com>
2025-04-10 08:50:29 +0200

    More comments

--- application.py
+++ application.py
@@ -1,6 +1,11 @@
+# Import the create_app function from your Capp package (__init__.py)
 from Capp import create_app
 
+# Call the factory function to create a Flask application instance
 application = create_app()
 
+# Run the application in development mode with debug enabled
+# This means the server will automatically reload if you make changes to the code,
+# and it will show detailed error messages in the browser when things go wrong.
 if __name__ == '__main__':
     application.run(debug=True)
